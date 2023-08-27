b524ef3e1f6479e3e8281af5040fd53c8bd2c818
zeekcheung <zeekcheung@gmail.com>
2023-08-27 22:10:36 +0800

    chore: upload env.ts

---
+++ src/utils/env.ts
@@ -0,0 +1,12 @@
+export type DevNodeEnv = 'dev' | 'development';
+export type ProdNodeEnv = 'prod' | 'production';
+export type DebugNodeEnv = 'debug';
+export type NodeEnv = DevNodeEnv | ProdNodeEnv | DebugNodeEnv;
+
+export const NODE_ENV = (process.env.NODE_ENV as NodeEnv) || 'dev';
+export const __DEV__ = NODE_ENV === 'dev' || NODE_ENV === 'development';
+export const __PROD__ = NODE_ENV === 'prod' || NODE_ENV === 'production';
+export const __DEBUG__ = NODE_ENV === 'debug';
+
+export const DEFAULT_ENV_FILE_PATH = '.env';
+export const ENV_FILE_PATH = `${DEFAULT_ENV_FILE_PATH}.${NODE_ENV}`;
